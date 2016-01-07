3dbd2297c14438e3b75a1cbf9d00b9a5cc66ad76
Kristoffer Richardsson <kristoffer@bitcraze.io>
2016-01-07 13:24:10 +0100

    Updated run-in-dev to use the public builder image

--- tools/run-in-dev.sh
+++ tools/run-in-dev.sh
@@ -4,8 +4,7 @@ set -e
 scriptDir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 rootDir=$scriptDir/..
 
-# TODO krri Change image
 # TODO krri Handle OSes, only need POLLING on non linux OSes
-cmd="docker run --rm -it --volume=${rootDir}:/module -p 80:4000 web-builder jekyll serve --host 0.0.0.0 --force_polling"
-echo $cmd
-$($cmd)
+cmd="docker run --rm -it --volume=${rootDir}:/module -p 80:4000 bitcraze/web-builder jekyll serve --host 0.0.0.0 --force_polling"
+echo "$cmd"
+echo "$($cmd)"
