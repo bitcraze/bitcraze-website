325a9eb3396c34bbf8c96027fb601cbb92b5f229
Kristoffer Richardsson <kristoffer@bitcraze.io>
2016-01-08 17:01:32 +0100

    Updated run-in-dev script for linux/osx

--- tools/run-in-dev.sh
+++ tools/run-in-dev.sh
@@ -4,7 +4,13 @@ set -e
 scriptDir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 rootDir=$scriptDir/..
 
-# TODO krri Handle OSes, only need POLLING on non linux OSes
-cmd="docker run --rm -it --volume=${rootDir}:/module -p 80:4000 bitcraze/web-builder jekyll serve --host 0.0.0.0 --force_polling"
+# On OSX we need to pull the file system
+if [ "$(uname)" == "Darwin" ]; then
+  extra_args="--force_polling"
+else
+  extra_args=""
+fi
+
+cmd="docker run --rm -it --volume=${rootDir}:/module -p 80:4000 bitcraze/web-builder jekyll serve --host 0.0.0.0 ${extra_args}"
 echo "$cmd"
-echo "$($cmd)"
+$cmd
