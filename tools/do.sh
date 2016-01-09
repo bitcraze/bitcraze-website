1ed9e4df94bb420376c0c3c4c6a62e2bcec2d1da
Kristoffer Richardsson <kristoffer@bitcraze.io>
2016-01-09 15:28:04 +0100

    Updated utility script
    Don't try to use port 80
    Renamed it to not confuse it with run-in-dev

--- tools/do.sh
+++ tools/do.sh
@@ -6,6 +6,6 @@ rootDir=$scriptDir/..
 
 tool=$1
 
-cmd="docker run --rm -it --volume=${rootDir}:/module -p 80:4000 bitcraze/web-builder ./tools/build/${tool}"
+cmd="docker run --rm -it --volume=${rootDir}:/module bitcraze/web-builder ./tools/build/${tool}"
 echo "$cmd"
 $cmd
