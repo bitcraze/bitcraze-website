084cf0a4d3d4cb59268891990736869ac2582a6f
Björn Mauritz <bjorn@bitcraze.io>
2016-06-17 14:39:48 +0200

    changed text for button

--- test/_plugins/stay_updated_test.rb
+++ test/_plugins/stay_updated_test.rb
@@ -8,7 +8,7 @@ class TestStayUpdated < Testbase
   def test_that_tag_is_rendered
     # Fixture
     tag = '{% stay_updated http://some.url %}'
-    expected = '<div class="row"><div class="col-md-12"><div class="text-center"><a href="http://some.url"><button type="button" class="buy-button"><i class="fa fa-envelope-o fa-fw"></i>Stay updated!</button></a></div></div></div>'
+    expected = '<div class="row"><div class="col-md-12"><div class="text-center"><a href="http://some.url"><button type="button" class="buy-button"><i class="fa fa-envelope-o fa-fw"></i>Keep me updated!</button></a></div></div></div>'
 
     # Test
     actual = Liquid::Template.parse(tag).render
