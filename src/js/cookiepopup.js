b2cc81b20cf7f5977db6e8ee803b43848928da65
Kristoffer Richardsson <kristoffer@bitcraze.io>
2017-08-24 14:17:31 +0200

    Touch up on cookie popup

--- src/js/cookiepopup.js
+++ src/js/cookiepopup.js
@@ -1,10 +1,34 @@
+/*
+This script is used to display a cookie policy popup. It is used on all bitcraze.io
+sites and must be self containes (not use any styling from css files).
+
+On www.bitcraze.io it is loaded and started from html, while it is loaded and
+triggered from Google Tag Manager on the other sub domains.
+
+Add this code snippet to GTM:
+
+var script = document.createElement('script');
+var prior = document.getElementsByTagName('script')[0];
+script.async = 1;
+script.src = "https://www.bitcraze.io/js/cookiepopup.js";
+script.onload = script.onreadystatechange = function( _, isAbort ) {
+    if(isAbort || !script.readyState || /loaded|complete/.test(script.readyState) ) {
+        script.onload = script.onreadystatechange = null;
+        script = undefined;
+
+        if(!isAbort) { cookiepopup.start(); }
+    }
+};
+prior.parentNode.insertBefore(script, prior);
+
+*/
+
 var cookiepopup = {
     name: "CookiePolicyAccepted",
     start: function() {
         if (navigator.cookieEnabled) {
             if (!cookiepopup.hasUserAcceptedPolicy()) {
-                console.log("Show warning");
-                document.body.innerHTML = '<div id="cookiePolicyPopup" style="line-height:1.2; position: fixed; bottom: 5px; right: 5px; width: 300px; background-color: black; color: green; font-family: monospace; font-size: larger; padding: 10px; border: 3px solid gray; z-index:999"> >> We use cookies to give you a good experience when visiting bitcraze.io. By using our site, you agree to our <a href="https://www.bitcraze.io/cookies/" target="_blank" style="color:#95C941">use of cookies</a> <br> <br><div style="line-height:1;"><a onclick="cookiepopup.acceptCookiePolicy();" style="color:#95C941; cursor: pointer; text-decoration: none;"> ---------------<br> | OK, got it! |<br> ---------------<br> </a></div></div> ' + document.body.innerHTML;
+                document.body.innerHTML = '<div id="cookiePolicyPopup" style="line-height:20px; position: fixed; bottom: 5px; right: 5px; width: 300px; background-color: black; color: green; font-family: monospace; font-size: 16px; padding: 10px; border: 3px solid gray; z-index:999"> >> We use cookies to give you a good experience when visiting bitcraze.io. By using our site, you agree to our <a href="https://www.bitcraze.io/cookies/" target="_blank" style="color:#95C941">use of cookies</a> <br> <br><div><a onclick="cookiepopup.acceptCookiePolicy();" style="color:#95C941; cursor: pointer; text-decoration: none;"> ---------------<br> | OK, got it! |<br> ---------------<br> </a></div></div> ' + document.body.innerHTML;
             }
         }
     },
@@ -23,4 +47,3 @@ var cookiepopup = {
         document.cookie = cookiepopup.name + "=;expires=" + date.toGMTString() + ";path=/;domain=bitcraze.io";
     },
 };
-window.onload = cookiepopup.start;
