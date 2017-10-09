d7c2cff6ac3f7ad22e552d6af4f86059da711e2d
Kristoffer Richardsson <kristoffer@bitcraze.io>
2017-10-09 15:43:05 +0200

    Added analytics event on video play

--- src/js/kraken.js
+++ src/js/kraken.js
@@ -7,6 +7,7 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 var kraken = {
   loadYoutubeVideo: function(element) {
     var id = $(element).attr('data-video-id');
+    ga('send', 'event', 'video', 'play', id);
     var player = new YT.Player(element, {
       videoId: id,
       playerVars: { 'autoplay': 1}
