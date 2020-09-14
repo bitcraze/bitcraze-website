b174ba3afb925d9ef7544324b5555bf255f24f3b
Kristoffer Richardsson <kristoffer@bitcraze.io>
2020-09-14 16:32:46 +0200

    Added check on cookie popup for crawlers to avoid getting the popup in google search results

--- src/js/cookiepopup.js
+++ src/js/cookiepopup.js
@@ -26,11 +26,17 @@ prior.parentNode.insertBefore(script, prior);
 var cookiepopup = {
     name: "CookiePolicyAccepted",
     start: function() {
-        if (navigator.cookieEnabled) {
+        if (navigator.cookieEnabled && !cookiepopup.isCrawler()) {
             if (!cookiepopup.hasUserAcceptedPolicy()) {
                 document.body.innerHTML = '<div id="cookiePolicyPopup" style="line-height:20px; position: fixed; bottom: 5px; right: 5px; width: 300px; background-color: black; color: green; font-family: monospace; font-size: 16px; padding: 10px; border: 3px solid gray; z-index:999"> >> We use cookies to give you a good experience when visiting bitcraze.io. By using our site, you agree to our <a href="https://www.bitcraze.io/cookies/" target="_blank" style="color:#95C941">use of cookies</a> <br> <br><div><a onclick="cookiepopup.acceptCookiePolicy();" style="color:#95C941; cursor: pointer; text-decoration: none;"> ---------------<br> | OK, got it! |<br> ---------------<br> </a></div></div> ' + document.body.innerHTML;
             }
         }
+    },
+    isCrawler: function() {
+        var botPattern = "/bot|google|baidu|bing|msn|teoma|slurp|yandex/i";
+        var re = new RegExp(botPattern, 'i');
+        var userAgent = navigator.userAgent;
+        return re.test(userAgent);
     },
     hasUserAcceptedPolicy: function () {
         return (document.cookie.indexOf(cookiepopup.name) != -1);
