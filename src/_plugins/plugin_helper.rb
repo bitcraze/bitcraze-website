95ca819ddcfc810c0baabd12dd0012b171e416aa
Kristoffer Richardsson <kristoffer@bitcraze.io>
2021-09-09 11:44:09 +0200

    Updated comments

--- src/_plugins/plugin_helper.rb
+++ src/_plugins/plugin_helper.rb
@@ -7,18 +7,18 @@ module Jekyll
         converter.convert(md)
       end
 
-      # Convert markdown and liquid. Use when adding content with liquid that was not part of the original source file.
-      # Nore: in unit tests it seems as this method returns the input string which makes it possible (dirty!) to verify
-      # what we send to it
+      # Process liquid tags. Use when adding content with liquid that was not part of the original source file.
       def liquidify(md, context)
         Liquid::Template.parse(md).render(context)
       end
 
+      # Split an argument string at ;
       def parse_args(str)
         parts = str.split(';')
         parts.map {|part| part.strip}
       end
 
+      # Storage for page wide data
       def page_plugin_data(context, plugin_name)
         if !context['page'].has_key?('bc_page_plugin_data')
           context['page']['bc_page_plugin_data'] = {}
@@ -31,6 +31,7 @@ module Jekyll
         context['page']['bc_page_plugin_data'][plugin_name]
       end
 
+      # Genereate id based on a title
       def generate_id(title)
         title.downcase.gsub(/[^a-z0-9 ]/, ' ').gsub(/\s+/,'-')
       end
